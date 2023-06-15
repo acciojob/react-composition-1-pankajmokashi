c4de019db06fb36c457c1b7c28539f9ab0a4c788
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-06-15 20:44:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,11 @@ const App = () => {
     <div>
         {/* Do not remove the main div */}
       <ul>
-        <li onClick={() => setTab("Tab 1")}>Tab 1</li>
-        <li onClick={() => setTab("Tab 2")}>Tab 2</li>
-        <li onClick={() => setTab("Tab 3")}>Tab 3</li>
+        <li onClick={() => setTab("This is the content for Tab 1.")}>Tab 1</li>
+        <li onClick={() => setTab("This is the content for Tab 2.")}>Tab 2</li>
+        <li onClick={() => setTab("This is the content for Tab 3.")}>Tab 3</li>
       </ul>
-      <p>
-        This is the content for {tab}.
-      </p>
+      <p>{tab}</p>
     </div>
   )
 }
