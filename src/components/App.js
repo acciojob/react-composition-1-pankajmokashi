68c4799dcdab38a5a36054ad6d4fa3228f9ac3e1
pankajmokashi <pankajmokasi12@gmail.com>
2023-07-17 11:43:51 +0530

    edited

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import React from "react";
 import './../styles/App.css';
 import Comp1 from "./Comp1.js"
 
-const tabs = [{titles: "Tab 1", contents: "This is the content for Tab 1."}, 
-        {titles: "Tab 2", contents: "This is the content for Tab 2."}, 
-        {titles: "Tab 3", contents: "This is the content for Tab 3."}]
+const tabs = [{title: "Tab 1", content: "This is the content for Tab 1."}, 
+        {title: "Tab 2", content: "This is the content for Tab 2."}, 
+        {title: "Tab 3", content: "This is the content for Tab 3."}]
 
 const App = () => {
   return (
--- src/components/Comp1.js
+++ src/components/Comp1.js
@@ -1,28 +1,22 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
-const Comp1 = ({tabs}) => {
-    let [tab, setTab] = useState("This is the content for tab 1.")
+const Comp1 = ({ tabs }) => {
+  let [tab, setTab] = useState("This is the content for tab 1.");
 
-    function changeHandle(content){
-        setTab(content)
-    }
+  function changeHandle(content) {
+    setTab(content);
+  }
 
-    return (
-        <div>
-            <ul>
-                {
-                    tabs.map((ele) =>(
-                        <li
-                            onClick={() => changeHandle(ele.contents)}
-                        >
-                            {ele.titles}
-                        </li>
-                    )) 
-                }
-            </ul>
-            <p>{tab}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul>
+        {tabs.map((ele) => (
+          <li onClick={() => changeHandle(ele.content)}>{ele.title}</li>
+        ))}
+      </ul>
+      <p>{tabs[tab].content}</p>
+    </div>
+  );
+};
 
-export default Comp1
+export default Comp1;
